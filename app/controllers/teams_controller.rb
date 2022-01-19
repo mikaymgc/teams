0870793a4b29ed9cf4540c13854ea8f850505feb
mikaymgc <mk25f.y@gmail.com>
2022-01-19 18:33:52 +0900

    チーム管理機能（仮実装）

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,7 @@
 class TeamsController < ApplicationController
+
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
   def index
     @teams = Team.all
   end
@@ -27,11 +30,16 @@ class TeamsController < ApplicationController
   end
 
   def destroy
+    redirect_to root_path if @team.destroy
   end
 
   private
 
   def team_params
     params.require(:team).permit(:team_name)
+  end
+
+  def set_team
+    @team = Team.find(params[:id])
   end
 end
